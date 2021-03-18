228c3dd2a86a8333a81cfcb2176930a82e29075b
saragutierrez15 <saragutierrez31@gmail.com>
2021-03-18 12:47:01 -0500

    pokemones en pantalla con estilo

--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
-//import { example } from './data.js';
-//import pokemones from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
 
 const pokemonEnPantalla = document.getElementById('pokemonEnPantalla');
-//const btnVer = document.getElementById ('verPokemon');
 
-
-  for(let i = 0;i<data.pokemon.length;i ++) {
-     let nombrePokemon = data.pokemon[i]['name'];
-     let numPokemon = data.pokemon[i]['num'];
-     let urlImagen = data.pokemon[i]['img'];
-     let imagen = new Image (150,150);
-     imagen.src = urlImagen;
-     let nuevoDiv = document.createElement ('div');
-     nuevoDiv.innerHTML = nombrePokemon +' ' + numPokemon;
-     pokemonEnPantalla.appendChild(nuevoDiv);
-     nuevoDiv.classList.add ('card');
-     nuevoDiv.insertAdjacentElement ('afterbegin',imagen);
+for(let i = 0;i<data.pokemon.length;i ++) {
+    let nombrePokemon = data.pokemon[i]['name'].toUpperCase();
+    let numPokemon = data.pokemon[i]['num'];
+    let urlImagen = data.pokemon[i]['img'];
+    let imagen = new Image (150,150);
+    imagen.src = urlImagen;
+    let divTitulo = document.createElement ('div')
+    divTitulo.innerHTML = nombrePokemon +' ' + numPokemon;
+    let nuevoDiv = document.createElement ('div');
+    nuevoDiv.appendChild (divTitulo)
+    divTitulo.classList.add ('cardTitulo')
+    pokemonEnPantalla.appendChild(nuevoDiv);
+    nuevoDiv.classList.add ('card');
+    nuevoDiv.insertAdjacentElement ('afterbegin',imagen);
   }
+ 
+
 
 
 
